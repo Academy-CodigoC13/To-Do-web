8435006a9787c825a7bf1f222e9f9496cf3eaf95
Yeffens <isrmosqueras2@gmail.com>
2023-08-29 08:54:57 -0500

    DOM 1 Y 2

--- script/script.js
+++ script/script.js
@@ -3,9 +3,33 @@
  de que la aplicación se cierre.*/
 
 // Codigo DOM #1
-
+document.querySelector('.new-todo').addEventListener('keyup', (event) => {
+    if (
+      event.keyCode === 13 &&
+      document.querySelector('.new-todo').value.length > 0
+    ) {
+      const item = document.querySelector('.new-todo')
+      //Llamar la función que crea la tarea.**
+    }
+  })
+  function agregarTarea(){
+    const tarea = document.querySelector('.new-todo')
+   tarea.push()
+   tarea.value = (agregarTarea)
+   
+  }
 // Codigo DOM #2
+// este fragmento permite conservar el estado del checkbox (true o false) en el localStorage
 
+function activateCheckboxListeners() {
+    const checkboxes = document.querySelectorAll('.toggle')
+    checkboxes.forEach((ch, i) => {
+      ch.addEventListener('click', () => {
+        itemsArray[i].checked = ch.checked
+        localStorage.setItem('items', JSON.stringify(itemsArray))
+      })
+    })
+  }
 // Codigo DOM #3
 
 // Codigo DOM #4
