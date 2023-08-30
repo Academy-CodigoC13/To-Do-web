6f1c57f80e538d7d819830511f979520cebc55f9
sorestrepo <so3049674@gmail.com>
2023-08-29 19:17:22 -0500

    se hicieron varios dom

--- script/script.js
+++ script/script.js
@@ -26,14 +26,109 @@ function displayFooter() {
 }
 
 // Codigo DOM #1
+document.querySelector('.new-todo').addEventListener('keyup', (event) => {
+  if (
+    event.keyCode === 13 &&
+    document.querySelector('.new-todo').value.length > 0
+  ) {
+    const item = document.querySelector('.new-todo')
+    //Llamar la función que crea la tarea.**
+    crearTarea(item.valiue)
+  }
+})
+function crearTarea(nombreTarea){
+  alert ("tarea completada")
+  
+
+}
 
 // Codigo DOM #2
+function activateCheckboxListeners() {
+  const checkboxes = document.querySelectorAll('.toggle')
+  const itemsArray = [
+    { description: "tarea 1", checked: false },
+    { description: "tarea 2", checked: true },
+  ]
+  checkboxes.forEach((ch, i) => {
+    ch.addEventListener('click', () => {
+      itemsArray[i].checked = ch.checked
+      localStorage.setItem('items', JSON.stringify(itemsArray))
+    })
+  })
+}
+activateCheckboxListeners()
+
 
 // Codigo DOM #3
+function eliminarTarea(index) {
+  const itemsArray = [
+    { description: 'Tarea 1', checked: false },
+    { description: 'Tarea 2', checked: true },
+  ]
+    itemsArray.splice(index, 1)
+    localStorage.setItem('items', JSON.stringify(itemsArray))
+}
+
+
+function activateDeleteListeners() {
+  let deleteBtn = document.querySelectorAll('.deleteBtn')
+  deleteBtn.forEach((db, i) => {
+    db.addEventListener('click', () => {
+      //Llamar la función que elimina la tarea
+      eliminarTarea(i);
+    })
+  })
+}
 
 // Codigo DOM #4
+function activateEditListeners() {
+  const editBtn = document.querySelectorAll('.editBtn')
+  const updateController = document.querySelectorAll('.update-controller')
+  const inputs = document.querySelectorAll('.input-controller textarea')
+  const prioritySelects = document.querySelectorAll(
+    '.edit-controller select'
+  )[0]
+  const categorySelects = document.querySelectorAll(
+    '.edit-controller select'
+  )[1]
+
+  editBtn.forEach((eb, i) => {
+    eb.addEventListener('click', () => {
+      updateController[i].style.display = 'block'
+      inputs[i].disabled = false
+
+      prioritySelects.value = itemsArray[i].priority
+      categorySelects.value = itemsArray[i].category
+    })
+  })
+
+  prioritySelects.addEventListener('change', (event) => {
+    const selectedIndex = event.target.selectedIndex
+    itemsArray[i].priority = event.target.options[selectedIndex].text
+    localStorage.setItem('items', JSON.stringify(itemsArray))
+  })
+
+  categorySelects.addEventListener('change', (event) => {
+    const selectedIndex = event.target.selectedIndex
+    itemsArray[i].category = event.target.options[selectedIndex].text
+    localStorage.setItem('items', JSON.stringify(itemsArray))
+  })
+}
 
 // Codigo DOM #5
+function guardarActualizacionTarea(index, nuevoTexto) {
+  const itemsArray = [
+    { description: 'Tarea 1', checked: false },
+    { description: 'Tarea 2', checked: true },
+  ];
+  itemsArray[index].description = nuevoTexto;
+  localStorage.setItem('items', JSON.stringify(itemsArray));
+}
+function activateSaveListeners() {
+  const saveBtn = document.querySelectorAll('.saveBtn')
+  const inputs = document.querySelectorAll('.input-controller textarea')
+  saveBtn.forEach((sB, i) => {
+    sB.addEventListener('click', () => {
 
 // Codigo DOM #6
 
