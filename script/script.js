6529afba471cc7584308847e7ed2699295d32343
roberthcm <castilloroberth92@gmail.com>
2023-09-01 10:07:23 -0500

    reinicio

--- script/script.js
+++ script/script.js
@@ -1,29 +1,9 @@
-/*TIPS: No olvides utilizar el almacenamiento local (localStorage)
+/*TIPS: *No olvides utilizar el almacenamiento local (localStorage)
  para que las tareas queden guardadas en caso
  de que la aplicación se cierre.*/
- 
-let itemsArray= localStorage.getItem("items") ?JSON.parse(localStorage.getItem ("items")) : []
-
-function crearTarea(nombreTarea) {
-  let objetoTarea = {
-    thing: nombreTarea,
-    chacked: false,
-    priority: "Alta",
-    category: "Casa",
-  };
-  itemsArray.push(objetoTarea);
-  localStorage.setItem("items", JSON.stringify(itemsArray))
-  location.reload()
-}
-
-function countPend() {
-  return 0;
-}
-
-
-
-function displayFooter() {
-  let page = `     
+ function displayFooter() {
+  let page = `      
+     
       <footer class="footer">
        
         <span class="todo-count"><strong>${countPend()}</strong> pendiente(s)</span>
@@ -40,108 +20,23 @@ function displayFooter() {
           </li>
         </ul>
         <button onclick="borrarCompletados()" id="clear-completed" class="clear-completed">Borrar completados</button>
-      </footer>`
+      </footer>
+    `
   document.querySelector('.footer').innerHTML = page
-  
 }
 
 // Codigo DOM #1
 
-document.querySelector('.new-todo').addEventListener('keyup', (event) => {
-  if (
-    event.keyCode === 13 &&
-    document.querySelector('.new-todo').value.length > 0
-  ) {
-    const item = document.querySelector('.new-todo')
-    //Llamar la función que crea la tarea.**
-  }
-})
 // Codigo DOM #2
-// este fragmento permite conservar el estado del checkbox (true o false) en el localStorage
 
-function activateCheckboxListeners() {
-    const checkboxes = document.querySelectorAll('.toggle')
-    checkboxes.forEach((ch, i) => {
-      ch.addEventListener('click', () => {
-        itemsArray[i].checked = ch.checked
-        localStorage.setItem('items', JSON.stringify(itemsArray))
-      })
-    })
-  }
 // Codigo DOM #3
-// Permite que la acción eliminar impacte el DOM del HTML, acá debes agegar algoritmo de eliminar tarea
 
-function activateDeleteListeners() {
-    let deleteBtn = document.querySelectorAll('.deleteBtn')
-    deleteBtn.forEach((db, i) => {
-      db.addEventListener('click', () => {
-        //Llamar la función que elimina la tarea
-        document.querySelectorAll('.deleteBtn')
-      })
-    })
-  }
 // Codigo DOM #4
 
-// Permite que la acción editar de las 2 listas desplegables "prioridad" y "categoría" impacte el DOM del HTML cuando cambies de opción, inserta este código tal cual, el reto está en saber en qué parte de tu código debes usarlo.
-
-function activateEditListeners() {
-    const editBtn = document.querySelectorAll('.editBtn')
-    const updateController = document.querySelectorAll('.update-controller')
-    const inputs = document.querySelectorAll('.input-controller textarea')
-    const prioritySelects = document.querySelectorAll(
-      '.edit-controller select'
-    )[0]
-    const categorySelects = document.querySelectorAll(
-      '.edit-controller select'
-    )[1]
-  
-    editBtn.forEach((eb, i) => {
-      eb.addEventListener('click', () => {
-        updateController[i].style.display = 'block'
-        inputs[i].disabled = false
-  
-        prioritySelects.value = itemsArray[i].priority
-        categorySelects.value = itemsArray[i].category
-      })
-    })
-  
-    // prioritySelects.addEventListener('change', (event) => {
-    //   const selectedIndex = event.target.selectedIndex
-    //   itemsArray[i].priority = event.target.options[selectedIndex].text
-    //   localStorage.setItem('items', JSON.stringify(itemsArray))
-    // })
-  
-    // categorySelects.addEventListener('change', (event) => {
-    //   const selectedIndex = event.target.selectedIndex
-    //   itemsArray[i].category = event.target.options[selectedIndex].text
-    //   localStorage.setItem('items', JSON.stringify(itemsArray))
-    // })
-  }
 // Codigo DOM #5
-//comentario del DOM 5
-function activateSaveListeners() {
-  const saveBtn = document.querySelectorAll('.saveBtn')
-  const inputs = document.querySelectorAll('.input-controller textarea')
-  saveBtn.forEach((sB, i) => {
-    sB.addEventListener('click', () => {
-      // Llamar la función que guarda la actualización la tarea
-      document.querySelectorAll('.saveBtn')
-    })
-  })
-}
+
 // Codigo DOM #6
-function activateCancelListeners() {
-  const cancelBtn = document.querySelectorAll('.cancelBtn')
-  const updateController = document.querySelectorAll('.update-controller')
-  const inputs = document.querySelectorAll('.input-controller textarea')
-  cancelBtn.forEach((cB, i) => {
-    cB.addEventListener('click', () => {
-      updateController[i].style.display = 'none'
-      inputs[i].disabled = true
-      inputs[i].style.border = 'none'
-    })
-  })
-}
+
 //El sistema debe permitir EDITAR o MODIFICAR una tarea.
 
 //El sistema debe permitir ELIMINAR una tarea.
