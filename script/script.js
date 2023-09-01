726d0e43932a284a11981cc12014839fdaf3a290
ferraohernandez <fferraohernandez@gmail.com>
2023-09-01 13:09:42 -0600

    Se hicieron algunos avances

--- script/script.js
+++ script/script.js
@@ -1,69 +1,92 @@
-/*TIPS: *No olvides utilizar el almacenamiento local (localStorage)
- para que las tareas queden guardadas en caso
- de que la aplicación se cierre.*/
-function displayFooter() {
-  let page = `      
-     
-      <footer class="footer">
-       
-        <span class="todo-count"><strong>${countPend()}</strong> pendiente(s)</span>
-        
-        <ul class="filters">
-          <li>
-            <a onclick="showAll() "class="selected filtro" href="#/">Todos</a>
-          </li>
-          <li>
-            <a onclick="showPend()" class="filtro" href="#/active">Pendientes</a>
-          </li>
-          <li>
-            <a onclick="showComp()" class="filtro" href="#/completed">Completados</a>
-          </li>
-        </ul>
-        <button onclick="borrarCompletados()" id="clear-completed" class="clear-completed">Borrar completados</button>
-      </footer>
-    `
-  document.querySelector('.footer').innerHTML = page
-}
-
-// Codigo DOM #1
 document.querySelector('.new-todo').addEventListener('keyup', (event) => {
   if (
     event.keyCode === 13 &&
     document.querySelector('.new-todo').value.length > 0
   ) {
-    const item = document.querySelector('.new-todo')
-    //Llamar la función que crea la tarea.**
+    const item = document.querySelector('.new-todo');
+    // Llamar la función que crea la tarea.
+    createTask(item.value);
+    item.value = ''; // Limpiar el campo de entrada.
   }
-})
+});
 
-// Codigo DOM #2
+function activateCheckboxListeners() {
+  const checkboxes = document.querySelectorAll('.toggle');
+  checkboxes.forEach((ch, i) => {
+    ch.addEventListener('click', () => {
+      itemsArray[i].checked = ch.checked;
+      localStorage.setItem('items', JSON.stringify(itemsArray));
+    });
+  });
+}
 
-// Codigo DOM #3
+function activateDeleteListeners() {
+  let deleteBtn = document.querySelectorAll('.deleteBtn');
+  deleteBtn.forEach((db, i) => {
+    db.addEventListener('click', () => {
+      // Llamar la función que elimina la tarea.
+      deleteTask(i);
+    });
+  });
+}
 
-// Codigo DOM #4
+function activateEditListeners() {
+  const editBtn = document.querySelectorAll('.editBtn');
+  const updateController = document.querySelectorAll('.update-controller');
+  const inputs = document.querySelectorAll('.input-controller textarea');
+  const prioritySelects = document.querySelectorAll(
+    '.edit-controller select'
+  )[0];
+  const categorySelects = document.querySelectorAll(
+    '.edit-controller select'
+  )[1];
 
-// Codigo DOM #5
+  editBtn.forEach((eb, i) => {
+    eb.addEventListener('click', () => {
+      updateController[i].style.display = 'block';
+      inputs[i].disabled = false;
 
-// Codigo DOM #6
+      prioritySelects.value = itemsArray[i].priority;
+      categorySelects.value = itemsArray[i].category;
+    });
+  });
 
-//El sistema debe permitir EDITAR o MODIFICAR una tarea.
+  prioritySelects.addEventListener('change', (event) => {
+    const selectedIndex = event.target.selectedIndex;
+    itemsArray[i].priority = event.target.options[selectedIndex].text;
+    localStorage.setItem('items', JSON.stringify(itemsArray));
+  });
 
-//El sistema debe permitir ELIMINAR una tarea.
+  categorySelects.addEventListener('change', (event) => {
+    const selectedIndex = event.target.selectedIndex;
+    itemsArray[i].category = event.target.options[selectedIndex].text;
+    localStorage.setItem('items', JSON.stringify(itemsArray));
+  });
+}
 
-//El sistema debe permitir AGREGAR una o varias tareas tarea.
+function activateSaveListeners() {
+  const saveBtn = document.querySelectorAll('.saveBtn');
+  const inputs = document.querySelectorAll('.input-controller textarea');
+  saveBtn.forEach((sB, i) => {
+    sB.addEventListener('click', () => {
+      // Llamar la función que guarda la actualización de la tarea.
+      saveTaskUpdate(i, inputs[i].value);
+      inputs[i].disabled = true;
+      updateController[i].style.display = 'none';
+    });
+  });
+}
 
-//El sistema deber permitir MARCAR una tarea como completada
+function activateCancelListeners() {
+  const cancelBtn = document.querySelectorAll('.cancelBtn');
+  const updateController = document.querySelectorAll('.update-controller');
+  const inputs = document.querySelectorAll('.input-controller textarea');
+  cancelBtn.forEach((cB, i) => {
+    cB.addEventListener('click', () => {
+      updateController[i].style.display = 'none';
+      inputs[i].disabled = true;
+      inputs[i].style.border = 'none';
+    });
+  });
+}
 
-//El sistema debe permitir dar diferentes PRIORIDADES a las tareas
-//EJEMPLO:
-
-//Sacar la basura - Prioridad: media
-
-//El sistema debe permitir visualizar tareas por CATEGORÍAS o ETIQUETAS
-//EJEMPLO:
-
-/*Categorías disponibles: PENDIENTE, COMPLETADO o TODASE.T.C */
-
-//Recordar llamar las funciones displayItems() y displayFooter() para mostrar
-//las tareas en pantalla
-displayFooter()
