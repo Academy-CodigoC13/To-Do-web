3910435b97cb90f3bb50f46a8d6bb3d65d4b7a37
MachadoAlejo <Machadoalejo7@gmail.com>
2023-09-05 23:00:07 -0500

    Para entregar

--- script/script.js
+++ script/script.js
@@ -17,7 +17,7 @@ function crearTarea (nombreTarea) {
 }
 
 function borrarTarea(posicion) {
-  itemsArray.splice(posicion, 1);
+  itemsArray.splice(posicion, 0);
   itemsArray = itemsArray.filter((tarea, i)=> i !== posicion && tarea);
 
   localStorage.setItem("items", JSON.stringify(itemsArray));
@@ -40,73 +40,6 @@ function countPend() {
 El siguiente código lo podrás usar para renderizar en tu front el panel de 
 cada tarea de tu aplicación
 */
-function displayItems() {
-  let items = ''
-  for (let i = 0; i < itemsArray.length; i++) {
-    items += `    <div class="item">
-                    <div class="input-controller">
-                      <input class="toogle" type="checkbox" id="check_${i}" ${
-      itemsArray[i].checked ? 'checked' : ''
-    } />
-                      <textarea disabled>${itemsArray[i].thing}</textarea>
-                      <div class="edit-controller">
-                        <div>
-                          Prioridad
-                          <select id="priority">
-                            <option ${
-                              itemsArray[i].priority === 'Alta'
-                                ? 'selected'
-                                : ''
-                            }>Alta</option>
-                            <option ${
-                              itemsArray[i].priority === 'Media'
-                                ? 'selected'
-                                : ''
-                            }>Media</option> 
-                            <option ${
-                              itemsArray[i].priority === 'Baja'
-                                ? 'selected'
-                                : ''
-                            }>Baja</option> 
-                          </select>
-                        </div>
-                        <div>
-                          Categorías
-                          <select id="category">
-                              <option ${
-                                itemsArray[i].category === 'Casa'
-                                  ? 'selected'
-                                  : ''
-                              }>Casa</option> 
-                              <option ${
-                                itemsArray[i].category === 'Trabajo'
-                                  ? 'selected'
-                                  : ''
-                              }>Trabajo</option> 
-                              <option ${
-                                itemsArray[i].category === 'Estilo de vida'
-                                  ? 'selected'
-                                  : ''
-                              }>Estilo de vida</option> 
-                            </select>
-                        </div>
-                        <i class="fa-solid fa-pen-to-square editBtn"></i>
-                        <i class="fa-solid fa-x deleteBtn"></i>
-                      </div>
-                    </div>
-                    <div class="update-controller">
-                    <button class="saveBtn">Save</button>
-                     <button class="cancelBtn">Cancel</button>
-                    </div>
-                  </div>`
-  }
-  document.querySelector('.todo-list').innerHTML = items
-  activateCheckboxListeners()
-  activateDeleteListeners()
-  activateEditListeners()
-  activateSaveListeners()
-  activateCancelListeners()
-}
 
 function displayFooter() {
   let page = `
